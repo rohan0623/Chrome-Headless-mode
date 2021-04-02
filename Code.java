7262cdaeabcfed72c5c5d05fea3b1615fcb2a873
Rohan0623 <61073876+Rohan0623@users.noreply.github.com>
2021-04-03 02:13:52 +0530

    Update Code.java

--- Code.java
+++ Code.java
@@ -7,9 +7,11 @@ ChromeOptions options = new ChromeOptions();
 options.addArguments("--window-size=1024,768");
 options.setHeadless(true);
 
+
 /*
 Headless chrome does not support all incoming languages and thus some pages do not respond well to that. Supporting the language that page outputs in, loads the page correctly. Add the below line to work it correctly in Chrome (Lnaguage- english(US)). 
 */
+
 options.addArguments("--lang=en_US");     
 driver = new ChromeDriver(options);
 driver.manage().window().maximize();
