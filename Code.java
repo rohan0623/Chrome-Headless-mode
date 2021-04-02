5356a579040b6c6b1e1b93e667da853ac0085528
Rohan0623 <61073876+Rohan0623@users.noreply.github.com>
2021-04-03 02:13:05 +0530

    Update and rename Code to Code.java

--- Code.java
+++ Code.java
@@ -6,6 +6,7 @@ System.setProperty("webdriver.chrome.driver", /path/to chromedriver);
 ChromeOptions options = new ChromeOptions();
 options.addArguments("--window-size=1024,768");
 options.setHeadless(true);
+
 /*
 Headless chrome does not support all incoming languages and thus some pages do not respond well to that. Supporting the language that page outputs in, loads the page correctly. Add the below line to work it correctly in Chrome (Lnaguage- english(US)). 
 */
